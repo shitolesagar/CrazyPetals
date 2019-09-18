a521f427286768d389d51137c7cf5097c3310c79
Rohan <rohan@plug-able.com>
2019-09-18 12:35:39 +0530

    basic changes

--- CrazyPetals.Repository/ApplicationDbContextFactory.cs
+++ CrazyPetals.Repository/ApplicationDbContextFactory.cs
@@ -11,7 +11,7 @@ namespace CrazyPetals.Repository
             var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
 
            //  builder.UseMySql("server=165.22.60.17;port=3306;uid=admin;password=Omni#1234$;database=CrazyPetalsDB_testing;",
-            builder.UseMySql("server=localhost;port=3306;uid=root;password=Reset1234;database=CrazyPetalsDB_testing;",
+            builder.UseMySql("server=localhost;port=3306;uid=root;password=Reset1234;database=CrazyPetalsDB_test;",
                 optionsBuilder => optionsBuilder.MigrationsAssembly(typeof(ApplicationDbContext).GetTypeInfo().Assembly.GetName().Name));
             return new ApplicationDbContext(builder.Options);
         }
