0e57f3cefbf1087782f4b07e0f994fd4897cd872
Rohan <rohan@plug-able.com>
2019-09-26 19:15:55 +0530

    peer review points

--- CrazyPetals.Entities/Constant/MessageConstants.cs
+++ CrazyPetals.Entities/Constant/MessageConstants.cs
@@ -6,23 +6,23 @@ namespace CrazyPetals.Entities.Constant
 {
     public static class MessageConstants
     {
-        public static string BannerAddSuccessMessage { get; } = "Banner Added Sucessfully.";
-        public static string CategoryAddSuccessMessage { get; } = "Category Added Sucessfully.";
-        public static string CategoryUpdateSuccessMessage { get; } = "Category Updated Sucessfully.";
-        public static string FilterAddSuccessMessage { get; } = "Filter Added Sucessfully.";
-        public static string FilterUpdateSuccessMessage { get; } = "Filter Updated Sucessfully.";
-        public static string NotificationAddSuccessMessage { get; } = "Notification Added Sucessfully.";
-        public static string CategoryDeleteSuccessMessage { get; } = "Category Deleted Sucessfully.";
-        public static string CategoryDeleteFailedMessage { get; } = "Category Deletion Failed. Make sure, there are no products with this category";
-        public static string FilterDeleteSuccessMessage { get; } = "Filter Deleted Sucessfully.";
+        public static string BannerAddSuccessMessage { get; } = "Banner added sucessfully.";
+        public static string CategoryAddSuccessMessage { get; } = "Category added sucessfully.";
+        public static string CategoryUpdateSuccessMessage { get; } = "Category updated sucessfully.";
+        public static string FilterAddSuccessMessage { get; } = "Filter added sucessfully.";
+        public static string FilterUpdateSuccessMessage { get; } = "Filter updated sucessfully.";
+        public static string NotificationAddSuccessMessage { get; } = "Notification added sucessfully.";
+        public static string CategoryDeleteSuccessMessage { get; } = "Category deleted sucessfully.";
+        public static string CategoryDeleteFailedMessage { get; } = "Category deletion failed. Make sure, there are no products with this category";
+        public static string FilterDeleteSuccessMessage { get; } = "Filter deleted aucessfully.";
         public static string FilterDeleteFailedMessage { get; } = "Something went wrong.";
-        public static string BannerDeleteSuccessMessage { get; } = "Banner Ad Deleted Sucessfully";
+        public static string BannerDeleteSuccessMessage { get; } = "Banner ad deleted sucessfully";
         public static string BannerDeleteFailedMessage { get; } = "Something went wrong.";
-        public static string NotificationDeleteSuccessMessage { get; } = "Notification Deleted Sucessfully";
+        public static string NotificationDeleteSuccessMessage { get; } = "Notification deleted sucessfully";
         public static string NotificationDeleteFailedMessage { get; } = "Something went wrong.";
-        public static object ProductAddSuccessMessage { get; set; } = "Product Added Sucessfully.";
-        public static object ProductUpdateSuccessMessage { get; set; } = "Product Updated Sucessfully.";
-        public static object ProductDeletedSuccessMessage { get; set; } = "Product Deleted Sucessfully.";
+        public static object ProductAddSuccessMessage { get; set; } = "Product added sucessfully.";
+        public static object ProductUpdateSuccessMessage { get; set; } = "Product updated sucessfully.";
+        public static object ProductDeletedSuccessMessage { get; set; } = "Product deleted sucessfully.";
         public static object ProductDeletionFailedMessage { get; set; } = "Something went wrong.";
         public static object DeliveryStatusUpdated { get; set; } = "Delivery status updated successfully.";
     }
